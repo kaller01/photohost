a17ee83dd239b7cda2ddbc3483f006b635ff53f0
kaller01 <martin@kallers.se>
2021-02-13 18:16:21 +0100

    Exif data

--- index.php
+++ index.php
@@ -39,6 +39,31 @@ if ($auth == "Token " . $secret) {
             $status = "The file has been uploaded.";
             $filename = htmlspecialchars(basename($_FILES["fileToUpload"]["name"]));
             $paths = resizeall($target_file,$filename);
+
+            
+
+            $photo = $target_file;
+            $exif = exif_read_data($photo, 0, true);
+            $height = $exif['COMPUTED']['Height'] ?? 300;
+            $width = $exif['COMPUTED']['Width'] ?? 200;
+            $date = strtotime($exif['EXIF']['DateTimeOriginal']) ?? null;
+
+            $f = $exif['COMPUTED']['ApertureFNumber'] ?? 'none';
+            $shutter = $exif['EXIF']['ExposureTime'] ?? 'none';
+            $rawLensData = $exif['EXIF']['UndefinedTag:0xA434'] ?? 'none';
+            if ($rawLensData === 'DT 0mm F0 SAM') {
+                $lens = "Canon EF 70-200 f4L";
+            } else if ($rawLensData === 'FE 16-35mm F4 ZA OSS') {
+                $lens = "Sony FE 16-35 F4";
+            } else if ($rawLensData === "20.7 mm") {
+                $lens = "DJI Phantom 3";
+            } else {
+                $lens = "Minolta 45mm F2";
+            }
+
+            $exif = ["height"=>$height,"width"=>$width,"date"=>$date,"shutterspeed"=>$shutter, "lens"=>$lens, "aperture"=>$f];
+
+
         } else {
             $status = "Sorry, there was an error uploading your file.";
             $statuscode = 500;
@@ -49,5 +74,5 @@ if ($auth == "Token " . $secret) {
     $status = "No auth";
 }
 
-echo json_encode(["status" => $status, "filename" => $filename, "paths" => $paths]);
+echo json_encode(["status" => $status, "filename" => $filename, "paths" => $paths, "exif"=>$exif]);
 http_response_code($statuscode);
